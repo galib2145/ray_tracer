773856726aba6bf735e0dc6a84500b4487497b11
Sharif Md. Saad Galib <galib@therapservices.net>
2015-02-04 13:37:44 +0600

    Test commit

--- main.cpp
+++ main.cpp
@@ -12,43 +12,50 @@
 
 #define MAX_RAY_DEPTH 5
 
+//ON MASTER BRANCH
+
 using namespace std;
 
-class Light_source{
-    public:
-        Vec3 position;
-        Vec3 color;
-        float intensity;
+class Light_source
+{
+public:
+    Vec3 position;
+    Vec3 color;
+    float intensity;
 
-        Light_source(Vec3 light_position, Vec3 light_color,float light_intensity){
-            this->position = light_position;
-            this->color = light_color;
-            this->intensity = light_intensity;
-        }
+    Light_source(Vec3 light_position, Vec3 light_color,float light_intensity)
+    {
+        this->position = light_position;
+        this->color = light_color;
+        this->intensity = light_intensity;
+    }
 };
 
-class RenderContext{
-    public:
-        Camera camera;
-        int image_width, image_height;
-        float aspect_ratio;
-        Vec3 background_color;
-        vector<Object*> objects;
-        vector<Light_source> light_sources;
+class RenderContext
+{
+public:
+    Camera camera;
+    int image_width, image_height;
+    float aspect_ratio;
+    Vec3 background_color;
+    vector<Object*> objects;
+    vector<Light_source> light_sources;
 
-        RenderContext(float width, float height, float fov, vector<Object*> objects, vector<Light_source>& input_light_sources){
-            image_height = height;
-            image_width = width;
-            aspect_ratio = (float)image_width/image_height;
-            background_color = Vec3(.39, .58, .93);
-            this->objects = objects;
-            cout<<"Render context : "<<input_light_sources.size()<<endl;
-            this->light_sources = input_light_sources;
-            cout<<"Render context : "<<light_sources.size()<<endl;
-        }
+    RenderContext(float width, float height, float fov, vector<Object*> objects, vector<Light_source>& input_light_sources)
+    {
+        image_height = height;
+        image_width = width;
+        aspect_ratio = (float)image_width/image_height;
+        background_color = Vec3(.39, .58, .93);
+        this->objects = objects;
+        cout<<"Render context : "<<input_light_sources.size()<<endl;
+        this->light_sources = input_light_sources;
+        cout<<"Render context : "<<light_sources.size()<<endl;
+    }
 };
 
-float get_diffused_color(Light_source light_source, Vec3 closest_hit_point, Vec3 normal, Object* object){
+float get_diffused_color(Light_source light_source, Vec3 closest_hit_point, Vec3 normal, Object* object)
+{
     Vec3 light_direction = light_source.position - closest_hit_point;
     light_direction.normalize();
     float diffused_cos_component = light_direction.dot(normal);
@@ -56,7 +63,8 @@ float get_diffused_color(Light_source light_source, Vec3 closest_hit_point, Vec3
     return light_source.intensity * diffused_cos_component;
 }
 
-float get_specular_color(Light_source light_source,Vec3 normal_at_hit, Vec3 closest_hit_point, Vec3 viewing_direction, Object* object){
+float get_specular_color(Light_source light_source,Vec3 normal_at_hit, Vec3 closest_hit_point, Vec3 viewing_direction, Object* object)
+{
     Vec3 light_direction = light_source.position - closest_hit_point;
     light_direction.normalize();
     Vec3 reflection_direction = (normal_at_hit * (normal_at_hit.dot(light_direction))) * 2.0 - light_direction;
@@ -67,16 +75,20 @@ float get_specular_color(Light_source light_source,Vec3 normal_at_hit, Vec3 clos
     return light_source.intensity * spec_float;
 }
 
-Vec3 trace(vector<Object*> objects, vector<Light_source> light_sources, Ray ray, Vec3 background_color, int depth){
+Vec3 trace(vector<Object*> objects, vector<Light_source> light_sources, Ray ray, Vec3 background_color, int depth)
+{
     Object* object_closest_hit;
     bool if_hit = false;
     float t_closest_hit = 1000;
     float t_hit;
 
-    for (int i=0; i<objects.size();i++){
+    for (int i=0; i<objects.size(); i++)
+    {
         Object* object = objects[i];
-        if(object->intersect(ray,t_hit)){
-            if(t_hit<t_closest_hit){
+        if(object->intersect(ray,t_hit))
+        {
+            if(t_hit<t_closest_hit)
+            {
                 if_hit = true;
                 t_closest_hit = t_hit;
                 object_closest_hit = object;
@@ -84,14 +96,16 @@ Vec3 trace(vector<Object*> objects, vector<Light_source> light_sources, Ray ray,
         }
     }
 
-    if(if_hit){
+    if(if_hit)
+    {
         Vec3 color(0);
         Vec3 closest_hit_point = ray.get_point(t_closest_hit);
         Vec3 normal_at_hit = object_closest_hit->get_normal_at_point(closest_hit_point);
         normal_at_hit.normalize();
         float specular_color_component = 0, diffused_color_component = 0;
 
-        for(int i=0;i<light_sources.size();i++){
+        for(int i=0; i<light_sources.size(); i++)
+        {
             Light_source light_source = light_sources[i];
             float diffused_color = get_diffused_color(light_source,closest_hit_point, normal_at_hit, object_closest_hit);
             Vec3 viewing_direction =  ray.origin - closest_hit_point;
@@ -106,7 +120,8 @@ Vec3 trace(vector<Object*> objects, vector<Light_source> light_sources, Ray ray,
     return background_color;
 }
 
-void render_image_from_buffer(char *buffer, RenderContext context){
+void render_image_from_buffer(char *buffer, RenderContext context)
+{
     std::ofstream ofs;
     ofs.open("./untitled.ppm");
     ofs << "P6\n" << context.image_width << " " << context.image_height << "\n255\n";
@@ -115,14 +130,17 @@ void render_image_from_buffer(char *buffer, RenderContext context){
     delete [] buffer;
 }
 
-void render(RenderContext context){
+void render(RenderContext context)
+{
     char *buffer = new char[context.image_width * context.image_height * 3];
     char *pix = buffer;
     memset(buffer, 0x0, sizeof(char) * context.image_width * context.image_height);
     Vec3 ray_origin;
 
-    for(int j=0;j<context.image_height;++j){
-        for(int i=0;i<context.image_width;i++,pix+=3){
+    for(int j=0; j<context.image_height; ++j)
+    {
+        for(int i=0; i<context.image_width; i++,pix+=3)
+        {
             float x_remapped = (2 * (i + 0.5) / context.image_width - 1) * context.camera.fov_range * context.aspect_ratio;
             float y_remapped = (1 - 2 * (j + 0.5) / context.image_height) * context.camera.fov_range * context.aspect_ratio;
             Vec3 ray_direction = context.camera.world_to_camera_matrix.mulitply_direction_by_matrix(Vec3(x_remapped,y_remapped,-4));
